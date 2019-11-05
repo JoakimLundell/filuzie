6cc33d3830869dbf79dc26ef7f7cbdddf0a7a1ef
Joakim Lundell <joakim.lundell@evry.com>
2019-11-05 14:33:03 +0100

    test register.php

--- register.php
+++ register.php
@@ -1,11 +1,17 @@
 <?php
 
+
 $values = '';
 foreach($_POST as $key=>$value) {
     $values.= $key . "=" . $value . "\t";
 }
 
+print $value;
+
 $filename = './rvsp/rvsp.txt';
 $handle = fopen($filename, 'a');
 fwrite($handle, $values);
 fclose($handle);
+
+
+return "Thanks";
